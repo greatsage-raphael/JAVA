f79426542f28617ffe81d71c45a091b44bd87067
Lubwama Collins <bizzicole87@example.com>
2024-09-17 08:51:18 +0300

    first commit

---
+++ exampleOne.java
@@ -0,0 +1,22 @@
+public class exampleOne{
+    public static void main (String[] args){
+
+        int numberOne = 10;
+        int numberTwo= 5;
+
+        int sum = numberOne + numberTwo;
+
+        int difference = numberOne - numberTwo;
+
+        int product = numberOne * numberTwo;
+
+        int quotient = numberOne / numberTwo;
+
+        System.out.println("Sum: "+sum);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Difference: " + difference);
+
+
+    }
+}
